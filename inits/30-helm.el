77cc1494897f7c45ab12b8031b891f36b940374e
buri17 <buri17@gmail.com>
2014-02-28 23:14:23 +0900

    Modify helm settings.

--- inits/30-helm.el
+++ inits/30-helm.el
@@ -2,3 +2,13 @@
 (require 'helm-config)
 (global-set-key (kbd "C-c h") 'helm-mini)
 (helm-mode 1)
+;; http://d.hatena.ne.jp/a_bicky/20140104/1388822688
+;; Emulate `kill-line' in helm minibuffer
+(setq helm-delete-minibuffer-contents-from-point t)
+(defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+  "Emulate `kill-line' in helm minibuffer"
+  (kill-new (buffer-substring (point) (field-end))))
+;; For find-file etc.
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;; For helm-find-files etc.
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
